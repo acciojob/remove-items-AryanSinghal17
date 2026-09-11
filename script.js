1a0dbbb0c3dd188ed04615f34cdca67f69f85b23
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-11 21:26:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btn = document.querySelector('input[type="button"]');
+
+
+btn.addEventListener("click", function () {
+	const col = document.getElementById("colorSelect");
+
+	col.remove(col.selectedIndex);
+})
